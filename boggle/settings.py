e98eadff48ef786e9f40349bca95d25ba9b4fef6
Yong Lian Hii <yonglian146@gmail.com>
2019-06-01 18:48:31 +0800

    move django heroku setup to  bottom of  settings.py

--- boggle/settings.py
+++ boggle/settings.py
@@ -13,7 +13,6 @@ import os
 
 if 'HEROKU' in os.environ:
     import django_heroku
-    django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +123,6 @@ GRID_SIZE = 4
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if 'HEROKU' in os.environ:
+    django_heroku.settings(locals())
